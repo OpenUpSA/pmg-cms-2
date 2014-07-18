9d0a30f5c7b6c3ff5e81b291aa45973457b2792f
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-07-18 13:17:23 +0200

    Add some missing config variables.

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,5 @@
 LOG_LEVEL = "DEBUG"
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/tmp.db'
+LOGGER_NAME = "pmg_cms_logger"  # make sure this is not the same as the name of the package to avoid conflicts with Flask's own logger
+SECRET_KEY = "AEORJAEONIAEGCBGKMALMAENFXGOAERGN"

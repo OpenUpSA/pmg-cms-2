7c89736b7cbb8c718686ee4fea8eedd91a0786e6
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-08-14 14:39:02 +0200

    Add links to files hosted on S3

--- backend/admin.py
+++ backend/admin.py
@@ -19,6 +19,29 @@ def _jinja2_filter_json(json_string):
     return tmp
 
 
+def _jinja2_filter_json_to_list(json_string):
+    out = []
+    try:
+        paths = []
+        tmp = json.loads(json_string)
+        for item in tmp:
+            if not item['filepath'] in paths:
+                out.append(item)
+                paths.append(item['filepath'])
+    except TypeError:
+        pass
+    return out
+
+
+def _jinja2_filter_strip_file(path_string):
+    tmp = ""
+    try:
+        tmp = "/".join(path_string.split("/")[1::])
+    except TypeError:
+        pass
+    return tmp
+
+
 def _jinja2_filter_convert_timestamp(unix_string):
     tmp = ""
     try:
@@ -45,7 +68,7 @@ class MyModelView(ModelView):
         title=macro('render_title'),
         _id=macro('render_json'),
         revisions=macro('render_json'),
-        files=macro('render_json'),
+        files=macro('render_files'),
         audio=macro('render_json'),
         minutes=macro('render_html'),
         meeting_date=macro('render_date'),
@@ -58,7 +81,7 @@ class MyModelView(ModelView):
 
 model_dict = models.generate_models()
 
-admin = Admin(app=app, name='PMG DATA', url='/')
+admin = Admin(app=app, name='PMG DATA', url='/', base_template='admin/my_master.html')
 
 model_names = model_dict.keys()
 model_names.sort()
@@ -68,5 +91,7 @@ for name in model_names:
 
 # add our function to the filters of the jinja2 environment
 app.jinja_env.filters['json'] = _jinja2_filter_json
+app.jinja_env.filters['json_to_list'] = _jinja2_filter_json_to_list
+app.jinja_env.filters['strip_file'] = _jinja2_filter_strip_file
 app.jinja_env.filters['convert_timestamp'] = _jinja2_filter_convert_timestamp
 

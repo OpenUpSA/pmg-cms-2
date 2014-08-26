77e75cfa0aa3f45662624b1ee3a3486e9232cb68
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-08-26 12:05:52 +0200

    Configure frontend for production.

--- config/production/config.py
+++ config/production/config.py
@@ -3,4 +3,5 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/tmp.db'
 LOGGER_NAME = "pmg_cms_logger"  # make sure this is not the same as the name of the package to avoid conflicts with Flask's own logger
 SECRET_KEY = "AEORJAEONIAEGCBGKMALMAENFXGOAERGN"
+API_HOST = "0.0.0.0:5005"
 RESULTS_PER_PAGE = 20

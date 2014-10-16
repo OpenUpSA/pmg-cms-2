c1db439a8141fae9faab6df957bf3ab6f961e199
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-10-16 12:49:09 +0200

    Updates to production config.

--- config/production/config.py
+++ config/production/config.py
@@ -3,7 +3,7 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/tmp.db'
 LOGGER_NAME = "pmg_cms_logger"  # make sure this is not the same as the name of the package to avoid conflicts with Flask's own logger
 SECRET_KEY = "AEORJAEONIAEGCBGKMALMAENFXGOAERGN"
-API_HOST = "0.0.0.0:5005"
+API_HOST = "0.0.0.0:5006"
 RESULTS_PER_PAGE = 20
 
 STATIC_HOST = "http://eu-west-1-pmg.s3-website-eu-west-1.amazonaws.com/"

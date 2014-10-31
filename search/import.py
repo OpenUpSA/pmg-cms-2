9c9e31180576da2f4419c431bd89914a3b0fc8da
Jason Norwood-Young <jason@10layer.com>
2014-10-31 13:36:55 +0200

    Moar search library

--- search/import.py
+++ search/import.py
@@ -1,4 +1,10 @@
-import search
+from search import Search
+import time
 
 if (__name__ == "__main__"):
+	start_time = time.time()
+	search = Search()
+	# search.drop("committee")
 	search.import_data("committee-meeting")
+	# search.import_content("committee")
+	print("--- %d seconds ---" % (time.time() - start_time))

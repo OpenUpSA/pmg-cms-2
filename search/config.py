b5ce67112526be05a06c39b9dd125bafdc44e17e
Jason Norwood-Young <jason@10layer.com>
2014-11-03 15:18:57 +0200

    Roll test and import into search, roll config and transform out of search, and make it a Real Boy

---
+++ search/config.py
@@ -0,0 +1,2 @@
+ES_SERVER = "http://localhost:9200"
+API_HOST = "http://localhost:5001"
---
+++ search/transforms.py
@@ -0,0 +1,23 @@
+class Transforms:
+	convert_rules = {
+		"committee": {
+			"id": "id",
+			"title": "name",
+			"description": ["info", "about"]
+		},
+		"committee-meeting": {
+			"id": "id",
+			"title": "title",
+			"description": ["content", 0, "summary"],
+			"fulltext": ["content", 0, "body"]
+		},
+		"member": {
+			"id": "id",
+			"title": "name",
+			"description": "bio"
+		},
+		"bill": {
+			"id": "id",
+			"title": "name"
+		}
+	}

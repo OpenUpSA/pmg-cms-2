be560d115758a370887aae7638d087218b1da34a
Jason Norwood-Young <jason@10layer.com>
2014-11-05 10:00:18 +0200

    Make sure that search config points to the proper servers

--- search/config.py
+++ search/config.py
@@ -1,2 +1,4 @@
-ES_SERVER = "http://localhost:9200"
-API_HOST = "http://localhost:5001"
+#ES_SERVER = "http://localhost:9200"
+#API_HOST = "http://localhost:5001"
+ES_SERVER = "http://ec2-54-77-69-243.eu-west-1.compute.amazonaws.com:9200"
+API_HOST = "http://pmg-cms.demo4sa.org"

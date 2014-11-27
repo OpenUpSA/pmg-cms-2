7aeb07713ff9419d18db1219dfa815ce3b8d4f9d
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-11-27 10:51:00 +0200

    Only allow editing of memberships from committee view.

--- backend/admin.py
+++ backend/admin.py
@@ -271,7 +271,14 @@ class MemberView(MyModelView):
         profile_pic_url=macro('render_profile_pic'),
         memberships=macro('render_committee_membership')
     )
-    form_columns = column_list
+    form_columns = (
+        'name',
+        'house',
+        'party',
+        'province',
+        'bio',
+        'profile_pic_url'
+    )
     form_overrides = dict(bio=fields.TextAreaField)
     form_ajax_refs = {
         'events': {

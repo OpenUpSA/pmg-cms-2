8d267a3459cb8e9a20c2a89ec0edebb7879148dd
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-12-03 16:26:06 +0200

    Add simple S3 upload function, using boto.

--- config/development/config.py
+++ config/development/config.py
@@ -6,4 +6,6 @@ FRONTEND_HOST = "http://localhost:5000/"
 RESULTS_PER_PAGE = 50
 
 STATIC_HOST = "http://eu-west-1-pmg.s3-website-eu-west-1.amazonaws.com/"
+ES_SERVER = "http://ec2-54-77-69-243.eu-west-1.compute.amazonaws.com:9200"
+S3_BUCKET = "eu-west-1-pmg"
 UPLOAD_PATH = "/tmp/pmg_upload/"

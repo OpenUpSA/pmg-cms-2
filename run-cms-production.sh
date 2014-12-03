8803208c8adfdc3a42ceab3269f70b826b4b4ce0
Greg Kempe <greg@kempe.net>
2014-12-03 12:11:44 +0200

    Second run script for frontend

--- run-cms-production.sh
+++ run-cms-production.sh
@@ -2,6 +2,6 @@
 set -e
 
 source env/bin/activate
-source production-cms-env.sh
+source production-env.sh
 
-/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 backend.app:app --bind 0.0.0.0:5005
+/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 backend.app:app --bind 0.0.0.0:5005 --log-file -

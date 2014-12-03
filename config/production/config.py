7cd1d8f75ab3e3c1ab817aa5a6b5f3f0936880ac
Jason Norwood-Young <jason@10layer.com>
2014-12-03 15:48:51 +0200

    ES_SERVER variable in config

--- config/production/config.py
+++ config/production/config.py
@@ -7,3 +7,4 @@ FRONTEND_HOST = "http://new.pmg.org.za/"
 RESULTS_PER_PAGE = 20
 
 STATIC_HOST = "http://eu-west-1-pmg.s3-website-eu-west-1.amazonaws.com/"
+ES_SERVER = "http://ec2-54-77-69-243.eu-west-1.compute.amazonaws.com:9200"

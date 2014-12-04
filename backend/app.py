ff624734f68d62ad7687891265e2649f6ff2d697
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-12-04 16:13:14 +0200

    Setup Flask-Mail.

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,14 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import sys
 import os
+from flask_mail import Mail
 
 env = os.environ.get('FLASK_ENV', 'development')
 
 app = Flask(__name__, static_folder="not_static")
 app.config.from_pyfile('../config/%s/config.py' % env)
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 # setup logging
 with open('config/%s/logging.yaml' % env) as f:

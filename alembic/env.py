cd4472303e0299cb5e2dbf3e4a65451bc9ebd83b
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2015-01-13 14:12:18 +0200

    Enable autogenerate support.

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,9 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import sys, os
+
+sys.path.append(os.getcwd())
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -13,9 +16,9 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+import backend.app
+from backend.models import db
+target_metadata = db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:

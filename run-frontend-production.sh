d04d9c5b38945165736634d5c2330b87bc8ec423
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2015-01-20 07:23:58 +0200

    Specify gunicorn timeout to avoid '[CRITICAL] WORKER TIMEOUT' in error logs.

--- run-frontend-production.sh
+++ run-frontend-production.sh
@@ -4,4 +4,4 @@ set -e
 source env/bin/activate
 source production-env.sh
 
-/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 frontend:app --bind 127.0.0.1:5006 --log-file -
+/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 frontend:app --bind 127.0.0.1:5006 --timeout 30 --log-file -

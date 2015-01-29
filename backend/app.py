9d300bc17c8df0c81564c00330281f1a95980657
Greg Kempe <greg@kempe.net>
2015-01-29 13:03:41 +0200

    Subaccounts for transactional and alert emails.

--- backend/app.py
+++ backend/app.py
@@ -18,5 +18,14 @@ with open('config/%s/logging.yaml' % env) as f:
     import yaml
     logging.config.dictConfig(yaml.load(f))
 
+
+# override flask mail's send operation to inject some customer headers
+original_send = mail.send
+def send_email_with_subaccount(message):
+    message.extra_headers = {'X-MC-Subaccount': app.config['MANDRILL_TRANSACTIONAL_SUBACCOUNT']}
+    original_send(message)
+app.extensions.get('mail').send = send_email_with_subaccount
+
+
 import views
 import admin

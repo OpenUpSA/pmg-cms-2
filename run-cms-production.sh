1a025e71a9b773e84fcdece33812535990227f5c
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2015-01-29 14:11:52 +0200

    Config: Allow long requests (600s) on backend for large file uploads.

--- run-cms-production.sh
+++ run-cms-production.sh
@@ -4,4 +4,4 @@ set -e
 source env/bin/activate
 source production-env.sh
 
-/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 backend.app:app --bind 127.0.0.1:5005 --timeout 30 --log-file -
+/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 backend.app:app --bind 127.0.0.1:5005 --timeout 600 --log-file -

ff80db65067728641df9c4497dc1d632cffd654e
Greg Kempe <greg@kempe.net>
2015-02-06 10:57:46 +0200

    Condense email alerts into one page.

--- frontend/user_management.py
+++ frontend/user_management.py
@@ -254,33 +254,32 @@ def update_current_user():
     return
 
 
-@app.route('/user/email-alerts/', methods=['GET', 'POST'])
+@app.route('/email-alerts/', methods=['GET', 'POST'])
 def email_alerts():
     """
     Allow a user to manage their notification alerts.
     """
+    committees = None
 
-    if not session.get('current_user'):
-        return redirect(url_for('login', next=request.url))
+    if session.get('current_user'):
+        if request.method == 'POST':
+            out = {'committee_alerts': [], 'general_alerts': []}
+            general_notifications = ['select-daily-schedule', ]
+            for field_name in request.form.keys():
+                if field_name in general_notifications:
+                    key = "-".join(field_name.split('-')[1::])
+                    out['general_alerts'].append(key)
+                else:
+                    committee_id = int(field_name.split('-')[-1])
+                    out['committee_alerts'].append(committee_id)
+            tmp = send_to_api('update_alerts', json.dumps(out))
+            if tmp:
+                flash("Your notification settings have been updated successfully.", "success")
+                return redirect(url_for('email_alerts'))
 
-    if request.method == 'POST':
-        out = {'committee_alerts': [], 'general_alerts': []}
-        general_notifications = ['select-daily-schedule', ]
-        for field_name in request.form.keys():
-            if field_name in general_notifications:
-                key = "-".join(field_name.split('-')[1::])
-                out['general_alerts'].append(key)
-            else:
-                committee_id = int(field_name.split('-')[-1])
-                out['committee_alerts'].append(committee_id)
-        tmp = send_to_api('update_alerts', json.dumps(out))
-        if tmp:
-            flash("Your notification settings have been updated successfully.", "success")
-            return redirect(url_for('email_alerts'))
+        committees = load_from_api('committee', return_everything=True)['results']
 
-    committee_list = load_from_api('committee', return_everything=True)
-    committees = committee_list['results']
-    return render_template('user_management/email_alerts.html', committees=committees, )
+    return render_template('user_management/email_alerts.html', committees=committees)
 
 
 @app.route('/committee-subscriptions/', methods=['GET', 'POST'])

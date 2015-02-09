29ad20a0437e202d0885ecf9b4745cf2c6c55317
Greg Kempe <greg@kempe.net>
2015-02-09 15:43:26 +0200

    Revert graceful restarts until it can be tested properly

--- run-cms-production.sh
+++ run-cms-production.sh
@@ -4,16 +4,4 @@ set -e
 source env/bin/activate
 source production-env.sh
 
-# catch SIGHUP and send to children, this tells gunicorn to restart gracefully
-trap sighup 1
-
-sighup() {
-  kill -HUP $child
-}
-
 /var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 backend.app:app --bind 127.0.0.1:5005 --timeout 600 --max-requests 20000 --max-requests-jitter 1000 --log-file -
-child=$!
-
-while true; do
-  wait $child
-done

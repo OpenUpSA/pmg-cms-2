8ebee3461202f97ca1d9e2be27d14032d13a2a66
Greg Kempe <greg@kempe.net>
2015-02-09 14:37:41 +0200

    Restart processes gracefully with SIGHUP
    We need to tell the bash processes explicitly to send the HUP to their
    children, otherwise they quit when they receive HUP.

--- run-cms-production.sh
+++ run-cms-production.sh
@@ -4,4 +4,16 @@ set -e
 source env/bin/activate
 source production-env.sh
 
+# catch SIGHUP and send to children, this tells gunicorn to restart gracefully
+trap sighup 1
+
+sighup() {
+  kill -HUP $child
+}
+
 /var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 backend.app:app --bind 127.0.0.1:5005 --timeout 600 --log-file -
+child=$!
+
+while true; do
+  wait $child
+done

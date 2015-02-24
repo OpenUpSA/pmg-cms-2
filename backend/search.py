9636210c45a0c1194be0000343d56884a14841b3
Greg Kempe <greg@kempe.net>
2015-02-24 11:46:37 +0200

    Don't send back highlights for unmatched fields.

--- backend/search.py
+++ backend/search.py
@@ -164,12 +164,12 @@ class Search:
                 "description": {
                     "fragment_size": 500,
                     "number_of_fragments": 1,
-                    "no_match_size": 250,
+                    "no_match_size": 0,
                     "tag_schema": "styled"},
                 "fulltext": {
                     "fragment_size": 500,
                     "number_of_fragments": 1,
-                    "no_match_size": 250,
+                    "no_match_size": 0,
                     "tag_schema": "styled"}}}
         self.logger.debug("query_statement: %s" % q)
         if (content_type):

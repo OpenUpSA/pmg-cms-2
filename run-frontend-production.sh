18f9b83a8546ff1bbcc14c17639f804a9b508e67
Greg Kempe <greg@kempe.net>
2015-03-20 16:49:15 +0200

    Up to 6 workers.

--- run-frontend-production.sh
+++ run-frontend-production.sh
@@ -4,4 +4,4 @@ set -e
 source env/bin/activate
 source production-env.sh
 
-/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 4 frontend:app --bind 127.0.0.1:5006 --timeout 30 --max-requests 2000 --max-requests-jitter 100 --pid gunicorn-frontend.pid --log-file -
+/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 6 frontend:app --bind 127.0.0.1:5006 --timeout 30 --max-requests 2000 --max-requests-jitter 100 --pid gunicorn-frontend.pid --log-file -

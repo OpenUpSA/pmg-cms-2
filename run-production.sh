df9092970ae45ec3909cb50ac9a13884bba0bbb0
Greg Kempe <greg@kempe.net>
2015-04-14 20:25:11 +0200

    Up the number of workers and lower the restart interval.
    We may need to deploy to a larger host.

--- run-production.sh
+++ run-production.sh
@@ -4,4 +4,4 @@ set -e
 source env/bin/activate
 source production-env.sh
 
-/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 6 pmg:app --bind 127.0.0.1:5006 --timeout 30 --max-requests 2000 --max-requests-jitter 100 --pid gunicorn.pid --log-file - --access-logfile -
+/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 8 pmg:app --bind 127.0.0.1:5006 --timeout 30 --max-requests 1500 --max-requests-jitter 100 --pid gunicorn.pid --log-file - --access-logfile -

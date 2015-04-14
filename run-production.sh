da244f82b89db1a9d6fa0c2dd97f5617ad24cc52
Greg Kempe <greg@kempe.net>
2015-04-14 22:27:11 +0200

    Use gevent and 4 threaded workers (1 thread each) for gunicorn
    This allows multiple concurrent requests per worker and fixes a few
    problems:
    
    a) thundering herds that would otherwise bring the site down
    b) issues where we exhausted workers because each website request
       translates to multiple API requests
    c) memory pressure from using 8 workers
    
    We may be able to reduce the workers to 2 or 3 at some point.

--- run-production.sh
+++ run-production.sh
@@ -4,4 +4,4 @@ set -e
 source env/bin/activate
 source production-env.sh
 
-/var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn -w 8 pmg:app --bind 127.0.0.1:5006 --timeout 30 --max-requests 1500 --max-requests-jitter 100 --pid gunicorn.pid --log-file - --access-logfile -
+exec /var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn --workers 4 --worker-class gevent --bind 127.0.0.1:5006 --timeout 30 --max-requests 3000 --max-requests-jitter 100 --pid gunicorn.pid --log-file - --access-logfile - pmg:app

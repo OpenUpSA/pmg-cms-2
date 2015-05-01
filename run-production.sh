c37b1709d6d55f56a01583597d324079c9080bdc
Greg Kempe <greg@kempe.net>
2015-05-01 11:10:01 +0200

    Reduce worker processes to 2.

--- run-production.sh
+++ run-production.sh
@@ -4,4 +4,4 @@ set -e
 source env/bin/activate
 source production-env.sh
 
-exec /var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn --workers 4 --worker-class gevent --bind 127.0.0.1:5006 --timeout 30 --max-requests 3000 --max-requests-jitter 100 --pid gunicorn.pid --log-file - --access-logfile - pmg:app
+exec /var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn --workers 2 --worker-class gevent --bind 127.0.0.1:5006 --timeout 30 --max-requests 3000 --max-requests-jitter 100 --pid gunicorn.pid --log-file - --access-logfile - pmg:app

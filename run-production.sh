613a4ae6fcee68bac282cee4483748e98989f80c
Greg Kempe <greg@kempe.net>
2015-07-13 17:14:00 +0200

    Remove run-production, gunicorn pid

--- run-production.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-source env/bin/activate
-source production-env.sh
-
-exec /var/www/pmg-cms/env/bin/newrelic-admin run-program /var/www/pmg-cms/env/bin/gunicorn --workers 2 --worker-class gevent --bind 127.0.0.1:5006 --timeout 30 --max-requests 3000 --max-requests-jitter 100 --pid gunicorn.pid --log-file - --access-logfile - pmg:app

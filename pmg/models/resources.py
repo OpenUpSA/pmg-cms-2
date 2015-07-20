3ce8f6f80f2ddb47616cd6d564f001664ac239f8
Damian <damian@code4sa.org>
2015-07-20 13:41:25 +0200

    Remove unused ChoiceType class

--- pmg/models/resources.py
+++ pmg/models/resources.py
@@ -1025,21 +1025,6 @@ class DailyScheduleFile(FileLinkMixin, db.Model):
     file = db.relationship('File', lazy='joined')
 
 
-class ChoiceType(types.TypeDecorator):
-
-    impl = types.String
-
-    def __init__(self, choices, **kw):
-        self.choices = dict(choices)
-        super(ChoiceType, self).__init__(**kw)
-
-    def process_bind_param(self, value, dialect):
-        return [k for k, v in self.choices.iteritems() if v == value][0]
-
-    def process_result_value(self, value, dialect):
-        return self.choices[value]
-
-
 class CommitteeMeetingAttendance(ApiResource, db.Model):
     __tablename__ = "committee_meeting_attendance"
     """

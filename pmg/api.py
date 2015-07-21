47ef9ed1e71b3a826157b3544771baddc7a0ec51
Damian <damian@code4sa.org>
2015-07-21 21:15:36 +0200

    Clean commented code

--- pmg/api.py
+++ pmg/api.py
@@ -330,6 +330,5 @@ def committee_meeting_attendance(committee_meeting_id):
     query = CommitteeMeetingAttendance.list()\
         .filter(CommitteeMeetingAttendance.meeting_id == committee_meeting_id)\
         .options(lazyload('member.memberships'))
-        # .options(joinedload('asked_by_member'))
 
     return api_resource_list('committee-meeting-attendance', None, query)

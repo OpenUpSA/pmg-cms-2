b262026b945a0a8532ef6ea253af1783bf9b4128
Greg Kempe <greg@kempe.net>
2015-10-06 11:59:46 +0200

    Refact mandrill sending into helper method

--- pmg/models/emails.py
+++ pmg/models/emails.py
@@ -3,8 +3,9 @@ import logging
 import datetime
 
 from sqlalchemy import func
+import mandrill
 
-from pmg import db
+from pmg import db, app
 
 
 log = logging.getLogger(__name__)
@@ -69,6 +70,43 @@ class SavedSearch(db.Model):
         us to send duplicate emails.
         """
         self.last_alerted_at = datetime.datetime.utcnow()
+
+        # TODO: don't send in development?
+
+        # TODO: get template
+        # TODO: build HTML from template
+        # TODO: send with mandrill
+
+        recipients = [{'email': r.email} for r in self.recipients]
+        merge_vars = [{"rcpt": r.email, "vars": [{"name": "NAME", "content": r.name or 'Subscriber'}]} for r in self.recipients]
+
+        # NBNBNBNB: the email template MUST have a special DIV in it to place the content in.
+        # This gets removed when importing the template into Mandrill from Mailchimp
+        #  <div mc:edit="main"></div>
+
+        template_vars = [
+            {"name": "main", "content": self.message.html},
+        ]
+
+        msg = {
+            "subject": self.message.subject,
+            "from_name": "PMG Notifications",
+            "from_email": self.message.sender,
+            "to": recipients,
+            "merge_vars": merge_vars,
+            "track_opens": True,
+            "track_clicks": True,
+            "preserve_recipients": False,
+            "google_analytics_campaign": self.template.utm_campaign,
+            "google_analytics_domains": ["pmg.org.za"],
+            "subaccount": app.config['MANDRILL_ALERTS_SUBACCOUNT'],
+        }
+
+        log.info("Email will be sent to %d recipients." % len(recipients))
+        log.info("Sending email via mandrill: %s" % msg)
+
+        mandrill_client = mandrill.Mandrill(app.config['MANDRILL_API_KEY'])
+        mandrill_client.messages.send_template(app.config["MANDRILL_ALERTS_TEMPLATE"], template_vars, msg)
         # TODO: send email
         db.session.commit()
 
@@ -111,3 +149,48 @@ class SavedSearch(db.Model):
             search.last_alerted_at = datetime.datetime.utcnow()
             db.session.add(search)
         return search
+
+
+def send_mandrill_email(subject, from_name, from_email, recipient_users, html, utm_campaign, subaccount=None):
+    """ Send an email using Mandrill, relying on Mandrill's templating system.
+
+    :param subject: email subject
+    :param from_name: name of the sender
+    :param from_email: email of the sender
+    :param recipient_users: array of `User` objects of recipients
+    :param html: HTML body of the email
+    :param utm_campaign: Google Analytics campaign (optional)
+    :param subaccount: Mandrill subaccount to use (optional)
+    """
+    subaccount = subaccount or app.config['MANDRILL_ALERTS_SUBACCOUNT']
+
+    recipients = [{'email': r.email} for r in recipient_users]
+    merge_vars = [{"rcpt": r.email, "vars": [{"name": "NAME", "content": r.name or 'Subscriber'}]} for r in recipient_users]
+
+    # NBNBNBNB: the email template MUST have a special DIV in it to place the content in.
+    # This gets removed when importing the template into Mandrill from Mailchimp
+    #  <div mc:edit="main"></div>
+
+    template_vars = [
+        {"name": "main", "content": html},
+    ]
+
+    msg = {
+        "subject": subject,
+        "from_name": from_name,
+        "from_email": from_email,
+        "to": recipients,
+        "merge_vars": merge_vars,
+        "track_opens": True,
+        "track_clicks": True,
+        "preserve_recipients": False,
+        "google_analytics_campaign": utm_campaign,
+        "google_analytics_domains": ["pmg.org.za"],
+        "subaccount": subaccount,
+    }
+
+    log.info("Email will be sent to %d recipients." % len(recipients))
+    log.info("Sending email via mandrill: %s" % msg)
+
+    mandrill_client = mandrill.Mandrill(app.config['MANDRILL_API_KEY'])
+    mandrill_client.messages.send_template(app.config["MANDRILL_ALERTS_TEMPLATE"], template_vars, msg)

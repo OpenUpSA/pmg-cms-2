d1a34de0f3e7bbe623fe586996b0ad176f43a001
Greg Kempe <greg@kempe.net>
2015-10-08 17:17:33 +0200

    Go back to sending emails at 3am UTC.

--- pmg/tasks.py
+++ pmg/tasks.py
@@ -20,5 +20,5 @@ def schedule():
     # day at 3am (UTC)
     job = scheduler.add_job('pmg.tasks:send_saved_search_alerts', 'cron',
                             id='send-saved-search-alerts', replace_existing=True,
-                            coalesce=True, hour=14, minute=10)
+                            coalesce=True, hour=3)
     log.info("Scheduled task: %s" % job)

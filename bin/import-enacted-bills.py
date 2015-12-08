4f2730d1420a5c313e5fe3e62b08a4b3ae9624ca
Greg Kempe <greg@kempe.net>
2015-12-08 11:10:47 +0200

    Handle edge cases

--- bin/import-enacted-bills.py
+++ bin/import-enacted-bills.py
@@ -10,6 +10,7 @@ nids = json.load(open('data/nid_url.json'))
 
 pages_by_nid = {p["nid"]: p for p in bill_pages}
 nids_by_url = {n["url"]: n for n in nids}
+nids = {n["nid"]: n for n in nids}
 
 DESC_RE = re.compile('s:\d+:"description";s:\d+:"([^"]*)";')
 
@@ -23,11 +24,20 @@ def get_description(s):
 
 def get_file_info(url):
     url = url.replace('http://www.pmg.org.za/', '')
-    nid = nids_by_url[url]["nid"]
+
+    if url.startswith('node/'):
+        nid = nids[url[5:]]["nid"]
+    else:
+        nid = nids_by_url.get(url)
+        if nid:
+            nid = nid["nid"]
+        else:
+            nid = {
+                u'bill/20060425-south-african-institute-for-drug-free-sport-amendment-act-25-2006': "44724",
+            }[url]
+
     page = pages_by_nid[nid]
     files = page["files"]
-    if len(files) > 1:
-        raise ValueError("More than one file for %s: %s" % (nid, files))
     f = files[0]
 
     f['description'] = get_description(f['field_file_bill_data'])
@@ -36,6 +46,11 @@ def get_file_info(url):
 
     return f
 
+
+added = 0
+missing = 0
+already_enacted = 0
+already_exists = 0
 
 # now load files into db
 not_found = []
@@ -48,12 +63,14 @@ for bill in bills:
     bill_obj = Bill.query.filter(Bill.year == year, Bill.number == number).join(BillType).filter(BillType.name == bill_type).first()
     if not bill_obj:
         print "Missing: %s %s -- %s" % (year, number, title)
+        missing += 1
         continue
 
     print "%s %s %s -- %s" % (bill_obj.id, year, number, title)
 
     # already have enacted?
     if any(v.enacted for v in bill_obj.versions):
+        already_enacted += 1
         print "Already have enacted, skipping"
 
     for version in (e for e in bill['entries'] if e['type'] == 'act'):
@@ -66,6 +83,7 @@ for bill in bills:
         existing = [bv for bv in bill_obj.versions if bv.file.file_path == info['filepath']]
         if existing:
             existing[0].enacted = True
+            already_exists += 1
             print "Already have matching file, skipping"
             continue
 
@@ -80,14 +98,26 @@ for bill in bills:
         if not file.title:
             file.title = info['description']
 
+        date = version['date']
+        # if no date, use the date it was signed
+        if not date or date == 'None':
+            events = [e for e in bill_obj.events if e.type == 'bill-signed']
+            events.sort(key=lambda e: e.date, reverse=True)
+            date = events[0].date
+
         bill_version = BillVersion()
-        bill_version.date = version['date']
+        bill_version.date = date
         bill_version.title = version['title']
         bill_version.file = file
         bill_version.enacted = True
         bill_version.bill = bill_obj
+        added += 1
         db.session.add(bill_version)
         db.session.flush()
-        1/0
 
-db.session.commit()
+print "added %d" % added
+print "missing %d" % missing
+print "already_enacted %d" % already_enacted
+print "already_exists %d" % already_exists
+
+# db.session.commit()

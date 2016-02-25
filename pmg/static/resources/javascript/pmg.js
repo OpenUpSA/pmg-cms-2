e1ee84835598ca0a0165a6cd470326984a5e27e7
Damian <damian@code4sa.org>
2016-02-25 12:19:59 +0200

    Add Google utm codes to elements on homepage

--- pmg/static/resources/javascript/pmg.js
+++ pmg/static/resources/javascript/pmg.js
@@ -81,6 +81,14 @@ $(function() {
       $target.addClass('expanded');
       $btn.addClass('show-less');
       $btn.text('Show less');
+      ga('send', 'event', 'show-more', 'click');
     }
   });
+
+  $('.search-form').submit(function(e) {
+    var medium = e.target.name;
+    ga('send', 'event', medium, 'search');
+    return true;
+});
+
 });

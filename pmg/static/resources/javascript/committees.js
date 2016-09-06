2bed9d67c1ff767e16d6acf7c70581cab02a1717
smmbll <lion@smmbll.com>
2016-09-06 14:25:56 +0200

    Added filter input

--- pmg/static/resources/javascript/committees.js
+++ pmg/static/resources/javascript/committees.js
@@ -1,4 +1,23 @@
-$('#committee-list a').click(function (e) {
+var $search = $('#committee-search');
+var $searchInput = $('#committee-search input');
+
+$('#committee-nav a').on('click', function (e) {
   e.preventDefault();
   $(this).tab('show');
 });
+
+$searchInput.on('focus', function() {
+  $(document).on('keypress', function(e) {
+    if(e.which === 13) {
+      if($searchInput.val().length > 0) {
+        $search.removeClass('has-error');
+      } else {
+        $search.addClass('has-error');
+      }
+    }
+  });
+});
+
+$searchInput.on('blur', function() {
+  $(document).off('keypress');
+})

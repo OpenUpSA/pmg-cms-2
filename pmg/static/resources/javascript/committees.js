8fed9f721fff2eafdf62d2641ee6435064ac613b
smmbll <lion@smmbll.com>
2016-09-09 12:27:05 +0200

    Switched to typeahead search

--- pmg/static/resources/javascript/committees.js
+++ pmg/static/resources/javascript/committees.js
@@ -9,7 +9,7 @@ var $committeesList = $('.committees-list');
 var $committeeNavItem = $('.committee-nav a, .committee-dd-nav a');
 var showingSearchResult = false;
 
-function clearSearchResult() {
+var clearSearchResult = function() {
   $searchResult.find('.left ul, .right ul')
     .empty();
   $noResults.hide();
@@ -17,6 +17,63 @@ function clearSearchResult() {
   $searchInput.val('');
   $committeesList.show();
 }
+
+// Underscore debounce
+var debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+var typeAhead = debounce(function() {
+  var value = $searchInput.val();
+  if(!!value.length) {
+    var $results = [];
+    var resultsCount = 0;
+
+    showingSearchResult = true;
+
+    $committeesList.hide();
+    $clearResults.show();
+
+    $committees.each(function() {
+      var $self = $(this);
+
+      if($self.find('a').text().toLowerCase().includes(value.toLowerCase())) {
+        $results.push($self.clone());
+      }
+    });
+
+    resultsCount = $results.length;
+
+    if(!!resultsCount) {
+      $resultsFound.show();
+
+      if(resultsCount >= 20) {
+        $searchResult.find('.left ul')
+          .append($results.slice(0,resultsCount / 2 - 1));
+        $searchResult.find('.right ul')
+          .append($results.slice(resultsCount,resultsCount - 1))
+      } else {
+        $searchResult.find('.left ul')
+          .append($results);
+      }
+    } else {
+      $noResults.show();
+    }
+  } else {
+    clearSearchResult();
+  }
+},500);
 
 $committeeNavItem.on('click', function (e) {
   e.preventDefault();
@@ -26,52 +83,13 @@ $committeeNavItem.on('click', function (e) {
   if(showingSearchResult) clearSearchResult();
 });
 
-$clearResults
-  .on('click', function() {
-    clearSearchResult();
-    $clearResults.hide();
-  });
+$clearResults.on('click', function() {
+  clearSearchResult();
+  $clearResults.hide();
+});
 
 $searchInput.on('focus', function() {
-  $(document).on('keypress', function(e) {
-    var value = $searchInput.val();
-    if(e.which === 13) {
-      var $results = [];
-      var resultsCount = 0;
-
-      showingSearchResult = true;
-
-      clearSearchResult();
-      $committeesList.hide();
-      $clearResults.show();
-
-      $committees.each(function() {
-        var $self = $(this);
-
-        if($self.find('a').text().toLowerCase().includes(value.toLowerCase())) {
-          $results.push($self.clone());
-        }
-      });
-
-      resultsCount = $results.length;
-
-      if(!!resultsCount) {
-        $resultsFound.show();
-
-        if(resultsCount >= 40) {
-          $searchResult.find('.left ul')
-            .append($results.slice(0,resultsCount / 2 - 1));
-          $searchResult.find('.right ul')
-            .append($results.slice(resultsCount,resultsCount - 1))
-        } else {
-          $searchResult.find('.left ul')
-            .append($results);
-        }
-      } else {
-        $noResults.show();
-      }
-    }
-  });
+  $searchInput.on('keyup', typeAhead);
 });
 
 $searchInput.on('blur', function() {

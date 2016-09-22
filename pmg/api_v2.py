38fda2a5329711c06c488aeabdd79c6fd28e7e2d
Greg Kempe <greg@kempe.net>
2016-09-22 08:19:29 +0200

    Simplified apiv2

--- pmg/api_v2.py
+++ pmg/api_v2.py
@@ -61,116 +61,74 @@ class MemberSchema(ma.ModelSchema):
         fields = ('id', 'name', 'profile_pic_url', 'party', 'pa_link', 'current')
 
 
-@api.route('/committees/')
-@api.route('/committees/<int:id>')
-def committees(id=None):
+def get_api_fields():
     fields = request.args.get('fields') or ''
     fields = [f for f in fields.split(',') if f]
     if not fields:
         fields = None
-
-    if id:
-        item = Committee.query.get(id)
-        if not item:
-            abort(404)
-
-        item, errors = CommitteeSchema(only=fields).dump(item)
-
-        out = {'result': item}
-
-    else:
-        base_query = Committee.list()
-        for f in get_filters():
-            base_query = base_query.filter_by(**f)
-
-        queryset, count, next = paginate_request_query(base_query)
-
-        results, errors = CommitteeSchema(many=True, only=fields).dump(queryset)
-
-        out = {
-            'count': count,
-            'next': next,
-            'results': results
-        }
-
-    return send_api_response(out)
+    return fields
 
 
-@api.route('/committees/<int:id>/meetings')
-def committee_meeting_list(id):
-    # TODO: fields, paginate, etc.
-
-    cte = Committee.query.get(id)
-    if not cte:
-        abort(404)
-
-    base_query = CommitteeMeeting.query.filter(CommitteeMeeting.committee_id == id).order_by(desc(CommitteeMeeting.date))
+def apply_filters(query):
     for f in get_filters():
-        base_query = base_query.filter_by(**f)
+        query = query.filter_by(**f)
+    return query
 
-    queryset, count, next = paginate_request_query(base_query)
 
-    results, errors = CommitteeMeetingSchema(many=True).dump(queryset)
-
+def api_list_items(query, schema):
+    query = apply_filters(query)
+    queryset, count, next = paginate_request_query(query)
+    results, errors = schema(many=True, only=get_api_fields()).dump(queryset)
     out = {
         'count': count,
         'next': next,
         'results': results
     }
-
     return send_api_response(out)
+
+
+def api_get_item(id, model, schema):
+    item = model.query.get(id)
+    if not item:
+        abort(404)
+
+    item, errors = schema(only=get_api_fields()).dump(item)
+    return send_api_response({'result': item})
+
+
+@api.route('/committees/')
+@api.route('/committees/<int:id>')
+def committees(id=None):
+    if id:
+        return api_get_item(id, Committee, CommitteeSchema)
+    else:
+        return api_list_items(Committee.list(), CommitteeSchema)
+
+
+@api.route('/committees/<int:id>/meetings')
+def committee_meeting_list(id):
+    cte = Committee.query.get(id)
+    if not cte:
+        abort(404)
+
+    query = CommitteeMeeting.query.filter(CommitteeMeeting.committee == cte).order_by(desc(CommitteeMeeting.date))
+    return api_list_items(query, CommitteeMeetingSchema)
 
 
 @api.route('/committee-meetings/')
 @api.route('/committee-meetings/<int:id>')
 def committee_meetings(id=None):
-    # TODO: fields, paginate, etc.
-
     if id:
-        item = CommitteeMeeting.query.filter(CommitteeMeeting.id == id).first()
-        if not item:
-            abort(404)
-
-        item, errors = CommitteeMeetingSchema().dump(item)
-        out = {'result': item}
-
+        return api_get_item(id, CommitteeMeeting, CommitteeMeetingSchema)
     else:
-        base_query = CommitteeMeeting.query.filter(CommitteeMeeting.committee_id == id).order_by(desc(CommitteeMeeting.date))
-        for f in get_filters():
-            base_query = base_query.filter_by(**f)
-
-        queryset, count, next = paginate_request_query(base_query)
-
-        results, errors = CommitteeMeetingSchema(many=True).dump(queryset)
-
-        out = {
-            'count': count,
-            'next': next,
-            'results': results
-        }
-
-    return send_api_response(out)
+        return api_list_items(CommitteeMeeting.list(), CommitteeMeetingSchema)
 
 
 @api.route('/committee-meetings/<int:id>/attendance')
 def committee_meeting_attendance(id):
-    # TODO: fields, paginate, etc.
     item = CommitteeMeeting.query.filter(CommitteeMeeting.id == id).first()
     if not item:
         abort(404)
 
-    base_query = CommitteeMeetingAttendance.query.filter(CommitteeMeetingAttendance.meeting_id == id)
-    for f in get_filters():
-        base_query = base_query.filter_by(**f)
-
-    queryset, count, next = paginate_request_query(base_query)
-
-    results, errors = CommitteeMeetingAttendanceSchema(many=True).dump(queryset)
-
-    out = {
-        'count': count,
-        'next': next,
-        'results': results
-    }
-
-    return send_api_response(out)
+    query = CommitteeMeetingAttendance.query.filter(CommitteeMeetingAttendance.meeting == item)
+    return api_list_items(query, CommitteeMeetingAttendanceSchema)

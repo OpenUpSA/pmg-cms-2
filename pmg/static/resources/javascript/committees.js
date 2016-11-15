72e1755f61840a64d9cbf272e571919e6b7cf70b
smmbll <lion@smmbll.com>
2016-11-15 10:27:53 +0200

    Megamenu now responds correctly to meetings being removed and added

--- pmg/static/resources/javascript/committees.js
+++ pmg/static/resources/javascript/committees.js
@@ -238,11 +238,27 @@ $cteList.on('change', '.cte-follow-committee input[type=checkbox]', function(e) 
         $listItemForm.attr('action','/user/unfollow/committee/' + id);
         $cteListUserFollowing.append($listItem.clone().attr('data-follow-list','true'));
 
+        // Attach to megamenu
+        var name = $listItem.find('.name').html();
         var $premium = $listItem.find('.premium').length ? '<span class="premium"><i class="fa fa-key"></i> Premium</span>' : '';
-        var $megaMenuItem = $('<li data-id="' + id + '" data-follow-list="true"><a href="/committee/' + id + '">' + $listItem.find('.name').html() + '</a>' + $premium + '</li>');
-        var $megaMenuItems = $megaMenuFollowing.find('li').append($megaMenuItem);
+        var $megaMenuItem = $('<li data-id="' + id + '" data-follow-list="true"><a href="/committee/' + id + '">' + name + '</a>' + $premium + '</li>');
+        var $megaMenuItems = $megaMenuFollowing.find('li');
 
-        $megaMenuItems.sort(function(a,b) {
+        $megaMenuItems.each(function(i) {
+          var thisName = $(this).find('a').html();
+          var nextName = $($megaMenuItems[i + 1]).find('a').html();
+          console.log(name,thisName,nextName,name < thisName,name < nextName);
+          if(name <= thisName) {
+            console.log('true');
+            $megaMenuItem.insertBefore($megaMenuItems[i]);
+            return false;
+          } else if((thisName < name && nextName >= name) || i == $megaMenuItems.length - 1) {
+            $megaMenuItem.insertAfter($megaMenuItems[i]);
+            return false;
+          }
+        });
+
+      /*  $megaMenuItems.sort(function(a,b) {
           if($(a).find('a').html() < $(b).find('a').html()) {
             return -1;
           } else {
@@ -254,9 +270,8 @@ $cteList.on('change', '.cte-follow-committee input[type=checkbox]', function(e) 
 
         var $items = $megaMenuItems.detach();
 
-        $megaMenuFollowing.append($items);
         $megaMenuFollowingMobile.empty()
-          .append($items);
+          .append($items);*/
       }
 
       if($cteListUserFollowing.find('li').length) {

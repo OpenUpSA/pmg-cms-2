3370f6d6da0a633ae0ece76fb1156f8d8761a77d
Greg Kempe <greg@kempe.net>
2016-11-30 20:58:18 +0200

    Remove unnecessary megamenu styling

--- pmg/static/resources/javascript/megamenu.js
+++ pmg/static/resources/javascript/megamenu.js
@@ -3,9 +3,6 @@ $(document).ready(function () {
 
     "use strict";
 
-    $('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
-    //Checks if li has sub (ul) and adds class for toggle icon - just an UI
-
 
     $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
     //Checks if drodown menu's li elements have anothere level (ul), if not the dropdown is shown as regular dropdown, not a mega menu (thanks Luka Kladaric)

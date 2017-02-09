693dcdbadd7ddce31825de63023e2b5bd2b222c7
JD Bothma <jbothma@gmail.com>
2017-02-09 15:30:13 +0200

    Revert "handle undocumented unfinished states storing and stored"

--- pmg/models/soundcloud_track.py
+++ pmg/models/soundcloud_track.py
@@ -9,11 +9,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 SOUNDCLOUD_ARTWORK_PATH = 'pmg/static/resources/images/logo-artwork.png'
-UNFINISHED_STATES = [
-    'storing',
-    'stored',
-    'processing',
-]
 
 
 class SoundcloudTrack(db.Model):
@@ -164,7 +159,7 @@ class SoundcloudTrack(db.Model):
     @classmethod
     def sync_upload_state(cls, client):
         tracks = db.session.query(cls) \
-                           .filter(cls.state.in_(UNFINISHED_STATES)) \
+                           .filter(cls.state == 'processing') \
                            .order_by(cls.created_at).all()
         for track in tracks:
             track.sync_state(client)

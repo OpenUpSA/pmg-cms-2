f0e35c39952fe500c19fbb03d9e25268250e4ca0
Guus Hoekman <guushoekman@gmail.com>
2017-05-08 13:30:01 +0200

    email friendly url with ampersand in title

--- pmg/static/resources/javascript/pmg.js
+++ pmg/static/resources/javascript/pmg.js
@@ -171,7 +171,8 @@ $(function() {
 
   $(".page-share.email").on("click", function(e) {
     e.preventDefault();
-    window.location = 'mailto:?subject=' + encodeURIComponent(document.title) + '&body=I saw this on pmg.org.za and thought it might interest you: ' + url;
+    var emailTitle = encodeURIComponent(document.title).replace(/&/g, '%26');
+    window.location = 'mailto:?subject=' + emailTitle + '&body=I saw this on pmg.org.za and thought it might interest you: ' + url;
     ga('send', 'social', 'email', 'share', url);
   });
 });

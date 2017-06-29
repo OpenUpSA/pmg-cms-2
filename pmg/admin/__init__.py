62d6f6f96cd0ddbf992d41fb2f0133b94de3f1bc
Guus Hoekman <guushoekman@gmail.com>
2017-06-29 16:49:53 +0200

    featured_date to date

--- pmg/admin/__init__.py
+++ pmg/admin/__init__.py
@@ -1072,11 +1072,11 @@ class BillStatusView(MyModelView):
 
 
 class PageView(ViewWithFiles, MyModelView):
-    column_list = ('slug', 'title', 'featured', 'featured_date')
+    column_list = ('slug', 'title', 'featured', 'date')
     column_searchable_list = ('slug', 'title')
     column_default_sort = 'slug'
 
-    form_columns = ('title', 'slug', 'path', 'body', 'featured', 'featured_date', 'show_files', 'files')
+    form_columns = ('title', 'slug', 'path', 'body', 'featured', 'date', 'show_files', 'files')
     form_extra_fields = {
         'path': fields.TextField('Path'),
     }

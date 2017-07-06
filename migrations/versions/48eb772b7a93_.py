702fb791941f1edaa275d983154e18242450c826
Guus Hoekman <mail@guushoekman.com>
2017-07-06 11:20:10 +0200

    merged migrations

---
+++ migrations/versions/48eb772b7a93_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 48eb772b7a93
+Revises: ('14b8fbcc1386', '4d46f8aeed5')
+Create Date: 2017-07-06 11:19:42.066678
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '48eb772b7a93'
+down_revision = ('14b8fbcc1386', '4d46f8aeed5')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

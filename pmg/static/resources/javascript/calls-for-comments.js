ace21b8b89d8740d503c94562f2336df2513e2f0
Guus Hoekman <mail@guushoekman.com>
2018-03-09 13:51:14 +0200

    no cards in search results, separate open and closed calls

--- pmg/static/resources/javascript/calls-for-comments.js
+++ pmg/static/resources/javascript/calls-for-comments.js
@@ -1,6 +1,9 @@
 var openCount = $(".call-for-comment-stub.open").length;
 $(".open-calls-count #count").text(openCount);
+if (openCount > 0) {
+  $(".open-calls-count").removeClass("hidden");
+};
 
 var lastOpen = $(".call-for-comment-stub.open").last();
 var lastOpenCol = $(lastOpen).parent().parent();
-$(lastOpenCol).after("<div class='col-xs-12 col-sm-6 col-lg-4'>");
+$(lastOpenCol).after("<div class='row'><div class='col-xs-12 col-lg-6'><div class='col-xs-12'><h3>It is not possible to share your input on closed calls for comments. However, you can still access the relevant contact information to send queries about a call for comment.</h3></div></div></div>");

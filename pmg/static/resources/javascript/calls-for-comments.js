d128923428a1b67e56b3963d14b51407811c1b72
Guus Hoekman <mail@guushoekman.com>
2018-03-12 16:44:52 +0200

    calls for comment cards like front page featured cards

--- pmg/static/resources/javascript/calls-for-comments.js
+++ pmg/static/resources/javascript/calls-for-comments.js
@@ -6,4 +6,4 @@ if (openCount > 0) {
 
 var lastOpen = $(".call-for-comment-stub.open").last();
 var lastOpenCol = $(lastOpen).parent().parent();
-$(lastOpenCol).after("<div class='row'><div class='col-xs-12 col-lg-6'><div class='col-xs-12'><h3>It is not possible to share your input on closed calls for comments. However, you can still access the relevant contact information to send queries about a call for comment.</h3></div></div></div>");
+$(lastOpenCol).after("<div class='row'><hr><div class='col-xs-12 col-lg-6'><div class='col-xs-12'><h2>Closed calls for comments</h2><h3>It is not possible to share your input on closed calls for comments. However, you can still access the relevant contact information to send queries about a call for comment.</h3></div></div></div>");

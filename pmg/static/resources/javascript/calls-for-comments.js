5cf9482e0613ff3cc097f755fd28412fa00ff4dd
Guus Hoekman <mail@guushoekman.com>
2018-03-14 11:24:24 +0200

    no js for dividing calls

--- pmg/static/resources/javascript/calls-for-comments.js
+++ pmg/static/resources/javascript/calls-for-comments.js
@@ -3,7 +3,3 @@ $(".open-calls-count #count").text(openCount);
 if (openCount > 0) {
   $(".open-calls-count").removeClass("hidden");
 };
-
-var lastOpen = $(".call-for-comment-stub.open").last();
-var lastOpenCol = $(lastOpen).parent().parent();
-$(lastOpenCol).after("<div class='row'><hr><div class='col-xs-12 col-lg-6'><div class='col-xs-12'><h2>Closed calls for comments</h2><h3>It is not possible to share your input on closed calls for comments. However, you can still access the relevant contact information to send queries about a call for comment.</h3></div></div></div>");

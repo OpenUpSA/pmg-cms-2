f489346e77d0e2022e6383da66fb5c485543cb14
JD Bothma <jbothma@gmail.com>
2018-04-19 13:11:25 +0200

    update test to use .test TLD

--- tests/api/test_committee_meetings.py
+++ tests/api/test_committee_meetings.py
@@ -10,11 +10,11 @@ class TestCommitteeMeetingsAPI(PMGTestCase):
         self.fx.setup()
 
     def test_premium_committee_meeting(self):
-        res = self.client.get('/committee-meeting/%s/' % self.fx.CommitteeMeetingData.premium_recent.id, base_url='http://api.pmg.dev:5000/')
+        res = self.client.get('/committee-meeting/%s/' % self.fx.CommitteeMeetingData.premium_recent.id, base_url='http://api.pmg.test:5000/')
         self.assertEqual(200, res.status_code)
         self.assertTrue(res.json['premium_content_excluded'])
 
     def test_premium_committee_meeting_old(self):
-        res = self.client.get('/committee-meeting/%s/' % self.fx.CommitteeMeetingData.premium_old.id, base_url='http://api.pmg.dev:5000/')
+        res = self.client.get('/committee-meeting/%s/' % self.fx.CommitteeMeetingData.premium_old.id, base_url='http://api.pmg.test:5000/')
         self.assertEqual(200, res.status_code)
         self.assertIsNone(res.json.get('premium_content_excluded'))

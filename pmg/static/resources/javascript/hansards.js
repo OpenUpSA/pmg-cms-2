f7af8c1209a67fd4e766a07d27ae44c9fb4c2343
Guus Hoekman <mail@guushoekman.com>
2018-04-26 11:29:29 +0200

    hansard filter with jquery.param

--- pmg/static/resources/javascript/hansards.js
+++ pmg/static/resources/javascript/hansards.js
@@ -1,15 +1,17 @@
 // select correct URL when selecting house or year
 $(".select-house select, .select-year form input").change(function() {
-  var filterHouse = "";
-  var filterYear = "";
   var selectedHouse = $(".select-house select").val();
   var selectedYear = $(".select-year input:checked").attr("year");
-  var ampersand = ""
-  if(selectedHouse) {
-    var filterHouse = "filter[house_id]=";
-        ampersand = "&";
-  }
-  if(selectedYear != "all")
-    var filterYear = "filter[year]=";
-  window.location.href = "/hansards/?" + filterHouse + selectedHouse + ampersand + filterYear + selectedYear;
+
+  var params = {};
+
+  if (selectedYear != "all") {
+    { params['filter[year]'] = selectedYear }
+  };
+
+  if (selectedHouse != "all") {
+    { params['filter[house_id]'] = selectedHouse }
+  };
+
+  window.location.href = "?" + jQuery.param(params);
 });

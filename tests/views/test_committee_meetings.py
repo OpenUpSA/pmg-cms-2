02fb318a2b9efdf0a1b193a8bbbc3198ef9ea4be
JD Bothma <jbothma@gmail.com>
2019-07-26 11:54:30 +0200

    Add some more failing test cases to debug DB issues

--- tests/views/test_committee_meetings.py
+++ tests/views/test_committee_meetings.py
@@ -17,3 +17,27 @@ class TestCommitteeMeetings(PMGLiveServerTestCase):
         )
         self.assertEqual(200, res.code)
         self.assertTrue(False)
+
+    def test_premium_committee_meeting3(self):
+        res = urllib2.urlopen(
+            "http://pmg.test:5000/committee-meeting/%s/"
+            % self.fx.CommitteeMeetingData.premium_recent.id
+        )
+        self.assertEqual(200, res.code)
+        self.assertTrue(False)
+
+
+class TestCommitteeMeetings2(PMGLiveServerTestCase):
+    def setUp(self):
+        super(TestCommitteeMeetings2, self).setUp()
+
+        self.fx = dbfixture.data(HouseData, CommitteeData, CommitteeMeetingData)
+        self.fx.setup()
+
+    def test_premium_committee_meeting2(self):
+        res = urllib2.urlopen(
+            "http://pmg.test:5000/committee-meeting/%s/"
+            % self.fx.CommitteeMeetingData.premium_recent.id
+        )
+        self.assertEqual(200, res.code)
+        self.assertTrue(False)

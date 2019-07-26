f723b94af055f2ed2e4d0da909842e5fc584857b
JD Bothma <jbothma@gmail.com>
2019-07-26 12:56:00 +0200

    Fix views package and clean up API test fixtures

--- tests/api/test_committee_meetings.py
+++ tests/api/test_committee_meetings.py
@@ -9,12 +9,22 @@ class TestCommitteeMeetingsAPI(PMGTestCase):
         self.fx = dbfixture.data(HouseData, CommitteeData, CommitteeMeetingData)
         self.fx.setup()
 
+    def tearDown(self):
+        self.fx.teardown()
+        super(TestCommitteeMeetingsAPI, self).tearDown()
+
     def test_premium_committee_meeting(self):
-        res = self.client.get('/committee-meeting/%s/' % self.fx.CommitteeMeetingData.premium_recent.id, base_url='http://api.pmg.test:5000/')
+        res = self.client.get(
+            "/committee-meeting/%s/" % self.fx.CommitteeMeetingData.premium_recent.id,
+            base_url="http://api.pmg.test:5000/",
+        )
         self.assertEqual(200, res.status_code)
-        self.assertTrue(res.json['premium_content_excluded'])
+        self.assertTrue(res.json["premium_content_excluded"])
 
     def test_premium_committee_meeting_old(self):
-        res = self.client.get('/committee-meeting/%s/' % self.fx.CommitteeMeetingData.premium_old.id, base_url='http://api.pmg.test:5000/')
+        res = self.client.get(
+            "/committee-meeting/%s/" % self.fx.CommitteeMeetingData.premium_old.id,
+            base_url="http://api.pmg.test:5000/",
+        )
         self.assertEqual(200, res.status_code)
-        self.assertIsNone(res.json.get('premium_content_excluded'))
+        self.assertIsNone(res.json.get("premium_content_excluded"))

75b0c843c33cb64e97c5ef9f43b3bf59c50c56ae
Delena Malan <delenamalan@gmail.com>
2019-07-26 16:20:03 +0200

    Add more committees tests

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -2,7 +2,7 @@ import pytz
 import datetime
 from fixture import DataSet, NamedDataStyle, SQLAlchemyFixture
 
-from pmg.models import db, House, Committee, CommitteeMeeting, Bill, BillType, Province, Party, CommitteeMeetingAttendance, Member
+from pmg.models import db, House, Committee, CommitteeMeeting, Bill, BillType, Province, Party, CommitteeMeetingAttendance, Member, CallForComment
 
 THIS_YEAR = datetime.datetime.today().year
 
@@ -113,6 +113,26 @@ class BillData(DataSet):
         year = 2019
         title = "Children's Amendment Bill"
         type = BillTypeData.private_member_bill_77
+
+class CallForCommentData(DataSet):
+    class arts_call_for_comment_one:
+        date = datetime.datetime(2019, 1, 1, 0, 0, 0, tzinfo=pytz.utc)
+        title = 'Feeds and Pet Food Bill - draft'
+        committee = CommitteeData.arts
+        start_date = datetime.datetime(2019, 1, 30, 0, 0, 0, tzinfo=pytz.utc)
+        end_date = datetime.datetime(2019, 4, 30, 0, 0, 0, tzinfo=pytz.utc)
+        body = """The Bill seeks to provide for:
+- regulation of feed and pet food,
+- regulation of feed ingredients used in the manufacturing of feed and pet food,
+- licensing or registration of facilities used for the manufacturing of feed or pet food,
+- appointment of a Registrar to administer the Act,
+- appointment of advisory committees
+- advisers, assignees, auditors, and inspectors to assist the Registrar in the exercise of his or her powers in the regulation, compliance monitoring and enforcement of this Act.
+
+Comments can be emailed to Mr Jonathan Mudzunga at MalutaM@daff.gov.za by no later than Tuesday, 9 April 2019.
+
+Enquiries can be directed to Mr Jonathan Mudzunga on tel (012) 319 7303"""
+        summary = 'The Department of Agriculture, Forestry and Fisheries has published the draft Feeds and Pet Food Bill, and is asking you to comment.'
 
 
 dbfixture = SQLAlchemyFixture(
--- tests/views/test_committees.py
+++ tests/views/test_committees.py
@@ -1,5 +1,7 @@
 from tests import PMGLiveServerTestCase
-from tests.fixtures import dbfixture, HouseData, CommitteeData, CommitteeMeetingData
+from tests.fixtures import (
+    dbfixture, HouseData, CommitteeData, CommitteeMeetingData, CallForCommentData
+)
 import urllib2
 
 
@@ -7,29 +9,56 @@ class TestCommittees(PMGLiveServerTestCase):
     def setUp(self):
         super(TestCommittees, self).setUp()
 
-        self.fx = dbfixture.data(HouseData, CommitteeData)
+        self.fx = dbfixture.data(
+            HouseData, CommitteeData, CommitteeMeetingData, CallForCommentData)
         self.fx.setup()
 
     def tearDown(self):
         self.fx.teardown()
         super(TestCommittees, self).tearDown()
 
-    def test_committees_page(self):
-        res = urllib2.urlopen(
-            "http://pmg.test:5000/committees"
-        )
-        html = res.read()
+    def get_page_contents(self, url):
+        res = urllib2.urlopen(url)
         self.assertEqual(200, res.code)
+        return res.read()
+
+    def test_committees_page(self):
+        html = self.get_page_contents("http://pmg.test:5000/committees")
         self.assertIn('committees', html)
         self.assertIn(self.fx.CommitteeData.communications.name, html)
         self.assertIn(self.fx.CommitteeData.arts.name, html)
         self.assertIn(self.fx.CommitteeData.constitutional_review.name, html)
+        self.assertIn(self.fx.HouseData.cjoint.name, html)
 
     def test_committee_page(self):
-        res = urllib2.urlopen(
+        committee = self.fx.CommitteeData.arts
+        html = self.get_page_contents(
             "http://pmg.test:5000/committee/%s/"
-            % self.fx.CommitteeData.communications.id
+            % committee.id
         )
-        html = res.read()
-        self.assertEqual(200, res.code)
-        self.assertIn(str(self.fx.CommitteeData.communications.id), html)
+        self.assertIn(committee.name, html)
+        headings = ['Committee meetings', 'Calls for comment',
+                    'Tabled reports', 'Questions and replies', 'Bills']
+        for heading in headings:
+            self.assertIn(heading, html)
+
+    def test_committee_page_meetings(self):
+        committee = self.fx.CommitteeData.arts
+        html = self.get_page_contents(
+            "http://pmg.test:5000/committee/%s/"
+            % committee.id
+        )
+
+        self.assertIn(
+            self.fx.CommitteeMeetingData.arts_meeting_one.title, html)
+        self.assertIn(
+            self.fx.CommitteeMeetingData.arts_meeting_two.title, html)
+
+    def test_committee_calls_for_comment(self):
+        committee = self.fx.CommitteeData.arts
+        html = self.get_page_contents(
+            "http://pmg.test:5000/committee/%s/"
+            % committee.id
+        )
+
+        self.assertIn(self.fx.CallForCommentData.arts_call_for_comment_one, html)

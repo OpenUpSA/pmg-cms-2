93efef469b0b00a3dae2a7c93de0168cb9ad78dd
Delena Malan <delenamalan@gmail.com>
2019-08-16 15:38:25 +0200

    Add admin bill edit and delete tests

--- tests/views/test_admin_bill_page.py
+++ tests/views/test_admin_bill_page.py
@@ -62,9 +62,10 @@ class TestAdminBillPage(PMGLiveServerTestCase):
         self.assertTrue(created_bill)
         self.created_objects.append(created_bill)
 
-    def test_admin_delete_bill(self):
+    def test_admin_action_bill(self):
         """
-        Delete a bill with the admin interface (http://pmg.test:5000/admin/bill/action/)
+        Delete a bill with the action url on the admin interface 
+        (http://pmg.test:5000/admin/bill/action/)
         """
         before_count = len(Bill.query.all())
         url = "http://pmg.test:5000/admin/bill/action/"
@@ -79,3 +80,45 @@ class TestAdminBillPage(PMGLiveServerTestCase):
         after_count = len(Bill.query.all())
         self.assertEqual(302, response.status_code)
         self.assertGreater(before_count, after_count)
+
+    def test_admin_delete_bill(self):
+        """
+        Delete a bill on the admin interface 
+        (http://pmg.test:5000/admin/bill/delete/)
+        """
+        before_count = len(Bill.query.all())
+        url = "http://pmg.test:5000/admin/bill/delete/"
+        data = {
+            'url': '/admin/bill/',
+            'id' : str(self.fx.BillData.food.id),
+            
+        }
+        response = self.request_as_user(self.user, url, data=data, method="POST")
+        after_count = len(Bill.query.all())
+        self.assertEqual(302, response.status_code)
+        self.assertGreater(before_count, after_count)
+
+    def test_admin_edit_bill(self):
+        """
+        Edit a bill with the admin interface (http://pmg.test:5000/admin/bill/edit/)
+        """
+        bill = db.session.query(Bill).first()
+        new_title = 'Cool Bill'
+        url = "http://pmg.test:5000/admin/bill/edit/?id=%d" % bill.id
+        data = {
+            'year': '2020',
+            'title': new_title,
+            'type': str(self.fx.BillTypeData.section_74.id),
+            'introduced_by': 'James',
+            'date_of_introduction': '2019-07-03',
+            'place_of_introduction': str(self.fx.HouseData.na.id),
+            'date_of_assent': '2019-07-03',
+            'effective_date': '2019-07-03',
+            'act_name': 'Fundamental',
+        }
+        response = self.request_as_user(self.user, url, data=data, 
+            method="POST", follow_redirects=True)
+        self.assertEqual(200, response.status_code)
+
+        db.session.refresh(bill)
+        self.assertEqual(new_title, bill.title)

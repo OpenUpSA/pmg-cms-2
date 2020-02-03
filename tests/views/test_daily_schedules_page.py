7308b8222c370b004349f1425b75b4d55b40e888
Delena Malan <delena.malan@gmail.com>
2020-02-03 09:11:04 +0200

    Add test for daily-schedule/<id> page

--- tests/views/test_daily_schedules_page.py
+++ tests/views/test_daily_schedules_page.py
@@ -1,24 +1,22 @@
 from tests import PMGLiveServerTestCase
-from tests.fixtures import (
-    dbfixture, HouseData, DailyScheduleData
-)
+from tests.fixtures import dbfixture, HouseData, DailyScheduleData
 
 
-class TestDailySchedulePage(PMGLiveServerTestCase):
+class TestDailySchedulesPage(PMGLiveServerTestCase):
     def setUp(self):
-        super(TestDailySchedulePage, self).setUp()
+        super(TestDailySchedulesPage, self).setUp()
 
         self.fx = dbfixture.data(DailyScheduleData)
         self.fx.setup()
 
     def tearDown(self):
         self.fx.teardown()
-        super(TestDailySchedulePage, self).tearDown()
+        super(TestDailySchedulesPage, self).tearDown()
 
     def test_daily_schedules_page(self):
         """
         Test daily schedules page (/daily-schedules/)
         """
         self.make_request("/daily-schedules/", follow_redirects=True)
-        self.assertIn('Daily Schedules', self.html)
+        self.assertIn("Daily Schedules", self.html)
         self.assertIn(self.fx.DailyScheduleData.schedule_one.title, self.html)

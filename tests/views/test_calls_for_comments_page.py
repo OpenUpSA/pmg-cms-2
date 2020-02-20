3aa8c6f67c8c7888413a3181c2a1cbade10858f4
Delena Malan <delena.malan@gmail.com>
2020-02-20 15:39:28 +0200

    Add test for calls for comments page

---
+++ tests/views/test_calls_for_comments_page.py
@@ -0,0 +1,24 @@
+from tests import PMGLiveServerTestCase
+from tests.fixtures import dbfixture, CallForCommentData
+import urllib.request, urllib.error, urllib.parse
+
+
+class TestCallsForCommentsPage(PMGLiveServerTestCase):
+    def setUp(self):
+        super(TestCallsForCommentsPage, self).setUp()
+
+        self.fx = dbfixture.data(CallForCommentData)
+        self.fx.setup()
+
+    def tearDown(self):
+        self.fx.teardown()
+        super(TestCallsForCommentsPage, self).tearDown()
+
+    def test_calls_for_comments(self):
+        """
+        Test calls for comments page (/calls-for-comments/)
+        """
+        call_for_comment = self.fx.CallForCommentData.arts_call_for_comment_one
+        self.make_request("/calls-for-comments/")
+        self.assertIn(call_for_comment.title, self.html)
+        self.assertTrue(False)

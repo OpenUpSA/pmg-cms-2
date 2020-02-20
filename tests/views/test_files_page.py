d405a1f95a19418537a1d28b3bf8a513bd35efdb
Delena Malan <delena.malan@gmail.com>
2020-02-20 10:40:57 +0200

    Mock only Tracker.send method

--- tests/views/test_files_page.py
+++ tests/views/test_files_page.py
@@ -1,11 +1,19 @@
 from tests import PMGLiveServerTestCase
 from tests.fixtures import dbfixture, HouseData, CommitteeData
+from universal_analytics import Tracker
 from unittest.mock import patch
 
 
+def mock_send(*args, **kwargs):
+    return True
+
 class TestFilesPage(PMGLiveServerTestCase):
-    @patch('pmg.views.utils.track_pageview')
-    def test_questions_file_page(self, mock_tracker):
+    def setUp(self):
+        super(TestFilesPage, self).setUp()
+        # self.app.config.set("GOOGLE_ANALYTICS_ID", 'test-id')
+
+    @patch.object(Tracker, 'send', mock_send)
+    def test_questions_file_page(self):
         """
         Test email alerts page (/questions/<path>)
         """

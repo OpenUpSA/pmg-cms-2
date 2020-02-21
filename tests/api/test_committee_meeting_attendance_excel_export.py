7879fc76908c672f54d93b69182e9d6ed47523c4
Delena Malan <delena.malan@gmail.com>
2020-02-21 14:43:26 +0200

    Test excel report

--- tests/api/test_committee_meeting_attendance_excel_export.py
+++ tests/api/test_committee_meeting_attendance_excel_export.py
@@ -1,3 +1,4 @@
+import xlrd
 from tests import PMGTestCase
 from tests.fixtures import dbfixture, CommitteeMeetingData
 
@@ -13,9 +14,14 @@ class TestCommitteeMeetingAttendanceExcelExport(PMGTestCase):
         self.fx.teardown()
         super(TestCommitteeMeetingAttendanceExcelExport, self).tearDown()
 
-    def test_get_committee_meeting_attendance_xlsx_smoke_test(self):
+    def test_get_committee_meeting_attendance_xlsx(self):
         res = self.client.get(
             "committee-meeting-attendance/data.xlsx",
             base_url="http://api.pmg.test:5000/",
         )
         self.assertEqual(200, res.status_code)
+
+        data = res.data
+        book = xlrd.open_workbook(file_contents=data)
+        self.assertEquals(1, book.nsheets)
+        self.assertEquals(["summary"], book.sheet_names())

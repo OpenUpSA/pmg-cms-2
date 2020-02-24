0db43d894bfb419a7f4b538f755af47fc0b653cb
Delena Malan <delena.malan@gmail.com>
2020-02-24 10:12:48 +0200

    Add mock response to sharpspring test

--- tests/unit/test_sharpspring.py
+++ tests/unit/test_sharpspring.py
@@ -4,13 +4,31 @@ from pmg.sharpspring import Sharpspring
 from tests import PMGTestCase
 
 
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def raise_for_status(self):
+        pass
+
+    def json(self):
+        return self.json_data
+
+
+def mocked_requests_post_success(*args, **kwargs):
+
+    return MockResponse({"result": {"creates": [{"success": True,}]}}, 200)
+
+
 class TestSharpspring(PMGTestCase):
-    @patch("pmg.sharpspring.requests.post")
+    @patch("pmg.sharpspring.requests.post", side_effect=mocked_requests_post_success)
     def test_make_sharpsrping_request(self, post_mock):
         sharpspring = Sharpspring()
         details = {
             "emailAddress": "test@example.com",
             "companyName": "Test Company",
         }
-        sharpspring.call("createLeads", {"objects": [details]})
+        result = sharpspring.call("createLeads", {"objects": [details]})
         post_mock.assert_called()
+        self.assertTrue(result["result"]["creates"][0])

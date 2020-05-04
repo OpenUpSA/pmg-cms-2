738bfff9216b3d80e2f8524eeffa6057952a275a
Delena Malan <delena.malan@gmail.com>
2020-05-04 14:44:55 +0200

    Add committee meeting date tests

--- tests/views/test_committee_meeting_page.py
+++ tests/views/test_committee_meeting_page.py
@@ -1,3 +1,4 @@
+import datetime
 from tests import PMGLiveServerTestCase
 from tests.fixtures import dbfixture, HouseData, CommitteeData, CommitteeMeetingData
 import urllib.request, urllib.error, urllib.parse
@@ -21,4 +22,5 @@ class TestCommitteeMeetingPage(PMGLiveServerTestCase):
         meeting = self.fx.CommitteeMeetingData.premium_recent
         self.make_request("/committee-meeting/%s/" % meeting.id)
         self.assertIn(meeting.title, self.html)
+        self.assertIn("5 November %d" % datetime.datetime.today().year, self.html)
         self.assertIn("Follow this committee", self.html)

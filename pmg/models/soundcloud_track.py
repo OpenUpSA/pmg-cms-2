c6e50c2983adddcdbc957015d84b0d13c47f2efa
Delena Malan <delena.malan@gmail.com>
2020-10-26 15:49:29 +0200

    Also log track uri

--- pmg/models/soundcloud_track.py
+++ pmg/models/soundcloud_track.py
@@ -204,7 +204,7 @@ class SoundcloudTrack(db.Model):
             try:
                 track.sync_state(client)
             except Exception as e:
-                logger.error("Soundcloud sync_state failed with: %s" % e)
+                logger.error("Soundcloud sync_state failed for %s with: %s" % (track.uri, e))
 
     @classmethod
     def handle_failed(cls, client):
